380040eef055842652a0c6c00750fd4fa30fb6fc
belgotech <belovedobiora2002@gmail.com>
2023-06-20 23:08:22 +0100

    Starting to structure the login

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import  React  from "react";
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+
+const Login = () =>{
+    return(
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to MercyChat</h2>
+
+                <div 
+                    className="login-button google"
+                >
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
