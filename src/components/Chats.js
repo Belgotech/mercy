966b5e93d9c1584e51f7e1f2752340e4c101b4a3
belgotech <belovedobiora2002@gmail.com>
2023-06-21 14:23:03 +0100

    Setting up chat

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../Firebase";
+import { Img } from "../Asset/Pink Dark Blue Modern Elegant Letter B Logo (1).png";
+
 
 const Chats = () =>{
     return(
-        <h1> chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    <img src={Img} alt="logo" />
+                </div>
+
+                <div className="log-out">
+                    Logout
+                </div>
+            </div>
+        </div>
     )
 }
 
